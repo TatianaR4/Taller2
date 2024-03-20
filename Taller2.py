acd0b47c863a6f45cb02c35025b0c352f38c4a4a
Heidy Romero <tatisrodriguez01427@gmail.com>
2024-03-20 15:17:08 -0500

    variable

--- Taller2.py
+++ Taller2.py
@@ -1,1 +1,20 @@
-#comentario 
+#comentario de una linea
+
+
+#Variables: Espacio de memoria,con nombre, donde guardo valores
+#Los nombres de variables deben ser cortos, descriptivos, NO TENER ESPACIOS
+#EN BLANCO ni caracteres especiales,no deben empezar por un numero
+
+#Descriptivo significa que representa la categoria del dato que quiero guardar
+#En python las variables pueden contener cualquier dato de tipo primitivo
+#números (enteros,reales), cadens de carcateres (string), booleanos (Tru,F)
+#carascters(letras)
+
+variable=1
+variable= 'Juventud divino tesoro,te vas para no volver,cuando quiero llorar no lloro'
+variable=True
+variable='a'
+variable=3.1415926535
+
+
+#Para asignar un valor a la varibale se usa el operador=
